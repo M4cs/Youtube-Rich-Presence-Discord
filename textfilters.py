9beee236f3e34ba0616dfbc478c7b130dceaf7d6
M4cs <maxlikescs@gmail.com>
2018-10-09 17:08:24 -0400

    Updated

---
+++ textfilters.py
@@ -0,0 +1,19 @@
+title_filter = [
+    "(",
+    ")",
+    "YouTube",
+    "Waterfox",
+    "Audio",
+    "Official Music Video",
+    "Official Audio",
+    "WSHH Exclusive",
+    "- ",
+    "-",
+    "Official",
+    " ",
+    "  ",
+    "   ",
+    "Google Chrome",
+    "Firefox",
+    "Internet Explorer"
+]
--- windows.py
+++ windows.py
@@ -1,6 +1,7 @@
 import ctypes
 from pypresence import Presence
 import time, os, sys
+from textfilters import title_filter
 
 client_id = ''  # Fake ID, put your real one here
 RPC = Presence(client_id)  # Initialize the client class
@@ -24,25 +25,10 @@ def full():
     sub = 'YouTube'
     for text in titles:
         if sub in text:
-            textp = text.replace("(", "")
-            textp2 = textp.replace(")", "")
-            textp3 = textp2.replace("YouTube", "")
-            textp4 = textp3.replace("Waterfox", "")
-            textp5 = textp4.replace("Audio", "")
-            textp6 = textp5.replace("Official Music Video", "")
-            textp7 = textp6.replace("Official Audio", "")
-            textp8 = textp7.replace("WSHH Exclusive", "")
-            textp9 = textp8.replace("- ", "")
-            textp10 = textp9.replace("-", "")
-            textp11 = textp10.replace("Official", "")
-            textp12 = textp11.replace("  ", "")
-            textp13 = textp12.replace("   ", "")
-            textp14 = textp13.replace("    ", "")
-            textp15 = textp14.replace("Google Chrome", "")
-            textp16 = textp15.replace("Firefox", "")
-            textp17 = textp16.replace("Internet Explorer", "")
+            for title in title_filter:
+                textp.replace(title,"")
 
-    print(RPC.update(state=textp17 + " on Youtube", details="Listening to: "))  # Set the presence
+    print(RPC.update(state=textp + " on Youtube", details="Listening to: "))  # Set the presence
     time.sleep(15) # Can only update rich presence every 15 seconds
     full()
 
