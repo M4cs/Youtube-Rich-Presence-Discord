ba7a4115453327b75192d093d9e56a3b5831d30f
Neko <24483290+LewdNeko@users.noreply.github.com>
2018-10-09 15:38:29 +0100

    Fix some things
    You shouldn't be recreating the Presence class each time - it's better to start it at the start of the program and then update it midway through.

--- windows.py
+++ windows.py
@@ -2,6 +2,10 @@ import ctypes
 from pypresence import Presence
 import time, os, sys
 
+client_id = ''  # Fake ID, put your real one here
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect() # Start the handshake loop
+
 def full():
     EnumWindows = ctypes.windll.user32.EnumWindows
     EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
@@ -38,13 +42,13 @@ def full():
             textp16 = textp15.replace("Firefox", "")
             textp17 = textp16.replace("Internet Explorer", "")
 
-    client_id = ''  # Fake ID, put your real one here
-    RPC = Presence(client_id)  # Initialize the client class
-    RPC.connect() # Start the handshake loop
     print(RPC.update(state=textp17 + " on Youtube", details="Listening to: "))  # Set the presence
-    while True:  # The presence will stay on as long as the program is running
-        time.sleep(15) # Can only update rich presence every 15 seconds
-        RPC.close()
-        full()
+    time.sleep(15) # Can only update rich presence every 15 seconds
+    full()
 
-full()
+try:
+    full()
+except KeyboardInterrupt:
+    print('Closing program')
+finally:
+    RPC.close()
